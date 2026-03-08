ac1af65b061a7b8a9babba68ae4a6e20e7917323
tare99 <tarearnaut@gmail.com>
2026-03-08 02:24:25 +0100

    Update docker-compose and SQL schema for payment table
    - Parameterize PostgreSQL environment variables
    - Add healthcheck for PostgreSQL service
    - Introduce Adminer service for database management
    - Rename payment_reference to payment_id in payment table
    - Update related index for payment_id

--- src/main/resources/db/migration/V2__create_payment_table.sql
+++ src/main/resources/db/migration/V2__create_payment_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE payment
 (
     id                  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    payment_reference   VARCHAR(30)    NOT NULL UNIQUE,
+    payment_id          VARCHAR(30)    NOT NULL UNIQUE,
     sender_account_id   BIGINT         NOT NULL REFERENCES account (id),
     receiver_account_id BIGINT         NOT NULL REFERENCES account (id),
     amount              DECIMAL(15, 2) NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE payment
 CREATE INDEX idx_payment_idempotency_key ON payment (idempotency_key);
 CREATE INDEX idx_payment_sender_account_id ON payment (sender_account_id);
 CREATE INDEX idx_payment_receiver_account_id ON payment (receiver_account_id);
-CREATE INDEX idx_payment_reference ON payment (payment_reference);
+CREATE INDEX idx_payment_id ON payment (payment_id);
 CREATE INDEX idx_payment_sender_status_created
     ON payment (sender_account_id, status, created_at);
 CREATE INDEX idx_payment_sender_status
