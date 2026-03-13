514f31f9d39d9a6a3954270881126bdb661a04c6
tare99 <tarearnaut@gmail.com>
2026-03-13 10:29:12 +0100

    Create open api

---
+++ src/main/java/io/github/tare99/paymentprocessor/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package io.github.tare99.paymentprocessor.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  private static final String BEARER_SCHEME = "bearerAuth";
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Payment Processor API")
+                .description(
+                    "Production-grade payment processing service. Obtain a demo JWT from POST /api/v1/auth/token.")
+                .version("1.0.0"))
+        .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    BEARER_SCHEME,
+                    new SecurityScheme()
+                        .name(BEARER_SCHEME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+  }
+}
